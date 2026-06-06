95cd303fef54e8b900dc1d4ef4b50c08afa04487
carolina <carolina@MONNIE>
2026-06-06 01:13:21 -0400

    finalizacion de clase excepcion para correo

--- src/CorreoInvalidoException.java
+++ src/CorreoInvalidoException.java
@@ -1,3 +1,17 @@
-public class CorreoInvalidoException {
+public class CorreoInvalidoException extends Exception {
+
+    public CorreoInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+
+    public static void Validar(String correo) throws CorreoInvalidoException{
+
+        if(!correo.contains("@") || !correo.contains(".")){
+
+            throw new CorreoInvalidoException("El correo no tiene el formato correcto.");
+        }
+    }
     
+    
+
 }
