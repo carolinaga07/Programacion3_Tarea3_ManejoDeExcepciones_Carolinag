3794c0f5fd383d82a3b5c8e7353b6b082e9c2b83
carolina <carolina@MONNIE>
2026-06-06 00:58:30 -0400

    se agregan las clases de las excepciones

--- src/UsuarioValidarExcepcion.java
+++ src/UsuarioValidarExcepcion.java
@@ -1,3 +1,7 @@
 public class UsuarioValidarExcepcion {
     
+    public static void validarUsuario(String nombre,int edad,String correo,double salario) throws NombreInvalidoException, EdadInvalidaException, CorreoInvalidoException, SalarioInvalidoException{
+
+        
+    }
 }
