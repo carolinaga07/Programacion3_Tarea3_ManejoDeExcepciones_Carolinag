d49da36c034a5f14641ea13b884b755e907a802a
carolina <carolina@MONNIE>
2026-06-06 01:03:52 -0400

    finalizacion de la excepcion nombre

--- src/NombreInvalidoException.java
+++ src/NombreInvalidoException.java
@@ -1,3 +1,21 @@
-public class NombreInvalidoException {
+public class NombreInvalidoException extends Exception {
+
+    public NombreInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+
+    public static void Validar(String nombre) throws NombreInvalidoException{
+
+        if ( nombre == null || nombre.isEmpty()){
+
+            throw new NombreInvalidoException("El nombre no puede estar vacio.");
+        }
+        if( nombre.length() < 3){
+
+             throw new NombreInvalidoException("El nombre debe tener al menos 3 caracteres.");
+        }
+    }
+
+    
     
 }
