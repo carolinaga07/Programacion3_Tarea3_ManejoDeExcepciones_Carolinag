820d9efa1426435b8fb159ec725cfb0b7dce7f69
carolina <carolina@MONNIE>
2026-06-06 00:43:05 -0400

    finalizacion de la clase usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,4 +1,51 @@
 public class Usuario {
+
+    private String Nombre;
+    private int Edad;
+    private String Correo;
+    private double Salario;
+   
+    public Usuario(String nombre, int edad, String correo, double salario) {
+        Nombre = nombre;
+        Edad = edad;
+        Correo = correo;
+        Salario = salario;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public void setEdad(int edad) {
+        Edad = edad;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    public double getSalario() {
+        return Salario;
+    }
+
+    public void setSalario(double salario) {
+        Salario = salario;
+    }
+
+    
+
     
     
 }
