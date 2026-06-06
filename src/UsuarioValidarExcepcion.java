74a14a51620478d6e33b8208904b88aab7b95186
carolina <carolina@MONNIE>
2026-06-06 01:18:06 -0400

    finalizacion de clase generica para excepciones

--- src/UsuarioValidarExcepcion.java
+++ src/UsuarioValidarExcepcion.java
@@ -2,6 +2,9 @@ public class UsuarioValidarExcepcion {
     
     public static void validarUsuario(String nombre,int edad,String correo,double salario) throws NombreInvalidoException, EdadInvalidaException, CorreoInvalidoException, SalarioInvalidoException{
 
-        
+        NombreInvalidoException.Validar(nombre);
+        EdadInvalidaException.Validar(edad);
+        CorreoInvalidoException.Validar(correo);
+        SalarioInvalidoException.Validar(salario);
     }
 }
