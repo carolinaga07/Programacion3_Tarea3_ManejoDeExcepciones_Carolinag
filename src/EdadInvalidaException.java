58c861e154c7d7bbdddc8dfecf632da3c98908a1
carolina <carolina@MONNIE>
2026-06-06 01:09:53 -0400

    finalizacion de clase de excepcion para edad

--- src/EdadInvalidaException.java
+++ src/EdadInvalidaException.java
@@ -1,3 +1,18 @@
-public class EdadInvalidaException {
+public class EdadInvalidaException  extends Exception{
+
+    public EdadInvalidaException( String mensaje) {
+        super(mensaje);
+    }
+
+
+    public static void Validar(int edad) throws EdadInvalidaException{
+
+        if( edad >= 18 || edad <= 100){
+
+             throw new EdadInvalidaException("La edad debe ser mayor o igual a 18 o menor o igual a 100.");
+        }
+    }
+
+    
     
 }
