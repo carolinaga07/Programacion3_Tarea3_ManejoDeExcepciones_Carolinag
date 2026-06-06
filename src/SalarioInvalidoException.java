2a6bcc4bb3a0dd9497ab5661542e537ee8f17bfe
carolina <carolina@MONNIE>
2026-06-06 01:15:57 -0400

    finalizacion de classe excepcion para salario

--- src/SalarioInvalidoException.java
+++ src/SalarioInvalidoException.java
@@ -1,3 +1,15 @@
-public class SalarioInvalidoException {
+public class SalarioInvalidoException extends Exception {
+
+    public SalarioInvalidoException(String mensaje) {
+        super(mensaje);
+    }
+
+    public static void Validar(double salario) throws SalarioInvalidoException{
+
+        if(salario <= 0){
+
+            throw new SalarioInvalidoException("El salario debe ser mayor a 0.");
+        }
+    }
     
 }
